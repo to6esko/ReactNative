2b7f6b1d6e47c4ec7ec1cdcc5f2c9383754a4947
Todor Krastev <to6esko69@gmail.com>
2017-04-03 18:28:00 +0300

    add ThouchableHighlight

--- myApp/app/components/component2/component2.js
+++ myApp/app/components/component2/component2.js
@@ -1,14 +1,70 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, Text, View, StyleSheet, TouchableHighlight, TouchableOpacity } from 'react-native';
 
 
 export default class Component2 extends Component {
-  render() {
-    return (
-        <View>
-        <Text>Hello from Component2!</Text>    
-      </View>
-    );
-  }
+    onPress() {
+        console.log('Area Pressed');
+    }
+    render() {
+        return (
+            <View>
+                <View style={styles.myView}>
+                    <Text style={styles.myText}>Hello from Component2!</Text>
+                </View>
+                <View style={styles.container}>
+                    <TouchableHighlight
+                        style={styles.v1}
+                        onPress={this.onPress}
+                        underlayColor="blue"
+                    >
+                    <View >
+                        <Text style={styles.vText}>View 1</Text>
+                    </View>
+                    </TouchableHighlight>    
+                    <View style={styles.v2}>
+                        <Text style={styles.vText}>View 2</Text>
+                    </View>
+                    <View style={styles.v3}>
+                        <Text style={styles.vText}>View 3</Text>
+                    </View>
+                </View>
+            </View>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    myView: {
+        backgroundColor: 'blue'
+    },
+    myText: {
+        color: 'white'
+    },
+    container: {
+        flexDirection: 'row',
+        height: 100
+    },
+    v1: {
+        flex: 1,
+        backgroundColor: 'red',
+        padding: 10
+    }
+    ,
+    v2: {
+        flex: 1,
+        backgroundColor: 'green',
+        padding: 10
+    }
+    ,
+    v3: {
+        flex: 1,
+        backgroundColor: 'black',
+        padding: 10
+    },
+    vText: {
+        color:'white'
+    }
+});
+
 AppRegistry.registerComponent('Component2', () => Component2);
