89a2b38081239b3725de1630a278247e3e831604
Todor Krastev <to6esko69@gmail.com>
2017-04-04 07:39:44 +0300

    fix ScrollView

--- myApp/app/components/component4/component4.js
+++ myApp/app/components/component4/component4.js
@@ -1,13 +1,62 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, Text, View, ListView, StyleSheet, ScrollView } from 'react-native';
+
+import Component5 from '../component5/component5';
+
+const users = [
+  { name: 'John Doe' },
+  { name: 'Todor Krastev' },
+  { name: 'Steve Smith' },
+  {name:'Janet Williams'}
+]
 
 export default class Component4 extends Component {
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      userDataSource: ds.cloneWithRows(users),
+    };
+  }
+
+  renderRow(user, sectionId, rowId, highlightRow) {
+    return (
+      <ScrollView>
+      <View style={styles.row}>
+        <Text style={styles.rowText}>{user.name}</Text>
+      </View>
+      <View>
+        <Component5 />  
+      </View>  
+      </ScrollView>  
+    );
+  }
+  
   render() {
     return (
       <View>
-        <Text>Component 4.</Text>
+        <ListView
+          dataSource={this.state.userDataSource}
+          renderRow={this.renderRow.bind(this)}  
+        />
       </View>
     );
   }
 }
+
+
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: '#f4f4f4',
+    marginBottom:3
+  },
+  rowText: {
+    flex:1
+  }
+});
+
 AppRegistry.registerComponent('Component4', () => Component4);
---
+++ myApp/app/components/component5/component5.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, ListView, StyleSheet, Navigator } from 'react-native';
+
+
+export default class Component5 extends Component {
+    constructor() {
+        super();
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.state = {
+            userDataSource: ds
+        };
+    }
+
+    componentDidMount() {
+        this.fetchUsers();
+    }
+    fetchUsers() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((response) => {
+                this.setState({
+                    userDataSource: this.state.userDataSource.cloneWithRows(response)
+                })
+            })
+    }
+
+    renderRow(user, sectionId, rowId, highlightRow) {
+        return (
+            <View style={styles.row}>
+                <Text style={styles.rowText}>{user.name}  email: {user.email}</Text>
+            </View>
+        );
+    }
+
+
+    render() {
+        return (
+                <ListView
+                    dataSource={this.state.userDataSource}
+                    renderRow={this.renderRow.bind(this)}
+                />
+        );
+    }
+}
+
+
+
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        padding: 10,
+        backgroundColor: '#f4f4f4',
+        marginBottom: 3
+    },
+    rowText: {
+        flex: 1
+    }
+});
+
+AppRegistry.registerComponent('Component5', () => Component5);
